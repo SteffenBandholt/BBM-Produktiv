254be8095833636a27b36dbf486698772a20eba2
SteffenBandholt <info@bandholt.de>
2026-08-09 12:53:12 +0200

    UI-Editor Registry fuer Protokoll-Counter auf Version 20 anheben

--- src/renderer/ui-editor/m80Registry.js
+++ src/renderer/ui-editor/m80Registry.js
@@ -11,7 +11,7 @@ import {
 } from "../ui/MainHeader.uiEditorContract.js";
 import { aggregateBbmM83Components } from "./m83ComponentContract.js";
 
-export const BBM_M80_REGISTRY_VERSION = 19;
+export const BBM_M80_REGISTRY_VERSION = 20;
 export const BBM_M80_REGISTRY_STATUS = "incomplete";
 
 export const BBM_M83_COMPONENT_CONTRACTS = Object.freeze([
