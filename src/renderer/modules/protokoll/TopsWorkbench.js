aa7d9429d2fa8b5a9e0df59aad1612499f99d9a9
SteffenBandholt <info@bandholt.de>
2026-08-09 12:23:20 +0200

    Protokoll Restzeichen-Counter an Editor-Refs binden

--- src/renderer/modules/protokoll/TopsWorkbench.js
+++ src/renderer/modules/protokoll/TopsWorkbench.js
@@ -1,4 +1,33 @@
-export { TopsWorkbench } from "../../tops/components/TopsWorkbench.js";
+import { TopsWorkbench as BaseTopsWorkbench } from "../../tops/components/TopsWorkbench.js";
+import { registerM80Ref } from "../../ui-editor/m80Refs.js";
+
+export class TopsWorkbench extends BaseTopsWorkbench {
+  constructor(options = {}) {
+    super(options);
+    this._installUiEditorCounterBinding();
+  }
+
+  _installUiEditorCounterBinding() {
+    const root = this.root;
+    const editbox = this.sharedEditboxCore?.editbox;
+    if (!root || !editbox) return;
+
+    const registerCounters = () => {
+      registerM80Ref("protokoll.edit.short.counter", editbox.shortCounter);
+      registerM80Ref("protokoll.edit.long.counter", editbox.longCounter);
+    };
+
+    Object.defineProperty(this, "root", {
+      configurable: true,
+      enumerable: true,
+      get() {
+        registerCounters();
+        return root;
+      },
+    });
+  }
+}
+
 export { SharedEditboxCore } from "./SharedEditboxCore.js";
 export { WorkbenchMetaColumn } from "./WorkbenchMetaColumn.js";
 export { WorkbenchShellFrame } from "./WorkbenchShellFrame.js";
