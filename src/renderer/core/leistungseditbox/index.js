ac40cd5d96709a570aea2025cedb79bcd8aa4513
SteffenBandholt <info@bandholt.de>
2026-08-26 17:48:58 +0200

    LeistungsEditbox Feldbauteil exportieren

--- src/renderer/core/leistungseditbox/index.js
+++ src/renderer/core/leistungseditbox/index.js
@@ -1,3 +1,4 @@
 export { LeistungsEditboxFrame, LEISTUNGSEDITBOX_FRAME_CLASS } from "./LeistungsEditboxFrame.js";
+export { LeistungsEditboxField, LEISTUNGSEDITBOX_FIELD_CLASS } from "./LeistungsEditboxField.js";
 export { createLeistungsEditboxPreview, PREVIEW_ROOT_ID, PREVIEW_SURFACE_ID, PREVIEW_FRAME_ID } from "./LeistungsEditboxPreview.js";
 export { ensureLeistungsEditboxStyles, STYLE_MARKER } from "./styles.js";
