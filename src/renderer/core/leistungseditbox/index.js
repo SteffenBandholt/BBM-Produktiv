a431d4b9cfc06818df7343a7527b439a59fc79f3
SteffenBandholt <info@bandholt.de>
2026-08-26 17:53:38 +0200

    LeistungsEditbox Kopf- und Aktionsbausteine exportieren

--- src/renderer/core/leistungseditbox/index.js
+++ src/renderer/core/leistungseditbox/index.js
@@ -1,4 +1,6 @@
 export { LeistungsEditboxFrame, LEISTUNGSEDITBOX_FRAME_CLASS } from "./LeistungsEditboxFrame.js";
 export { LeistungsEditboxField, LEISTUNGSEDITBOX_FIELD_CLASS } from "./LeistungsEditboxField.js";
+export { LeistungsEditboxHeader, LEISTUNGSEDITBOX_HEADER_CLASS } from "./LeistungsEditboxHeader.js";
+export { LeistungsEditboxAction, LEISTUNGSEDITBOX_ACTION_CLASS } from "./LeistungsEditboxAction.js";
 export { createLeistungsEditboxPreview, PREVIEW_ROOT_ID, PREVIEW_SURFACE_ID, PREVIEW_FRAME_ID } from "./LeistungsEditboxPreview.js";
 export { ensureLeistungsEditboxStyles, STYLE_MARKER } from "./styles.js";
