5b2e42a1c4d907be1f48807d0624ad189736e4e7
SteffenBandholt <info@bandholt.de>
2026-08-26 16:05:31 +0200

    LeistungsEditbox Preview exportieren

--- src/renderer/core/leistungseditbox/index.js
+++ src/renderer/core/leistungseditbox/index.js
@@ -1,1 +1,3 @@
 export { LeistungsEditboxFrame, LEISTUNGSEDITBOX_FRAME_CLASS } from "./LeistungsEditboxFrame.js";
+export { createLeistungsEditboxPreview, PREVIEW_ROOT_ID, PREVIEW_SURFACE_ID, PREVIEW_FRAME_ID } from "./LeistungsEditboxPreview.js";
+export { ensureLeistungsEditboxStyles, STYLE_MARKER } from "./styles.js";
