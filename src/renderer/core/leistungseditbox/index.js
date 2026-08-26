2bd3fe767bfa05b2991944ac201f2d4c4bda979b
SteffenBandholt <info@bandholt.de>
2026-08-26 18:02:29 +0200

    LeistungsEditbox Layoutbausteine exportieren

--- src/renderer/core/leistungseditbox/index.js
+++ src/renderer/core/leistungseditbox/index.js
@@ -2,5 +2,11 @@ export { LeistungsEditboxFrame, LEISTUNGSEDITBOX_FRAME_CLASS } from "./Leistungs
 export { LeistungsEditboxField, LEISTUNGSEDITBOX_FIELD_CLASS } from "./LeistungsEditboxField.js";
 export { LeistungsEditboxHeader, LEISTUNGSEDITBOX_HEADER_CLASS } from "./LeistungsEditboxHeader.js";
 export { LeistungsEditboxAction, LEISTUNGSEDITBOX_ACTION_CLASS } from "./LeistungsEditboxAction.js";
+export {
+  LeistungsEditboxRow,
+  LeistungsEditboxGroup,
+  LEISTUNGSEDITBOX_ROW_CLASS,
+  LEISTUNGSEDITBOX_GROUP_CLASS,
+} from "./LeistungsEditboxLayout.js";
 export { createLeistungsEditboxPreview, PREVIEW_ROOT_ID, PREVIEW_SURFACE_ID, PREVIEW_FRAME_ID } from "./LeistungsEditboxPreview.js";
 export { ensureLeistungsEditboxStyles, STYLE_MARKER } from "./styles.js";
