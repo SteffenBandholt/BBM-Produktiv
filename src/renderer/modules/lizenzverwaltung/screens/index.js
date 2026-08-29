a735c544762d6f1a74da99f9761a37c1e8a014e7
SteffenBandholt <info@bandholt.de>
2026-08-29 09:52:42 +0200

    Neue Lizenzoberfläche aktivieren

--- src/renderer/modules/lizenzverwaltung/screens/index.js
+++ src/renderer/modules/lizenzverwaltung/screens/index.js
@@ -1,4 +1,7 @@
-export { default as LicenseAdminScreen } from "./LicenseAdminScreen.js";
+export { default as LicenseAdminScreen } from "./LicenseAdminScreenV2.js";
+
+// Bestehender Export bleibt für Altpfäde/Tests erhalten; die sichtbare
+// Oberfläche verwendet ab jetzt LicenseAdminScreenV2.
 export { buildCustomerSetupPayload } from "./LicenseAdminScreen.js";
 
 export const LIZENZVERWALTUNG_WORK_SCREEN_ID = "licenseAdmin";
